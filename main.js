7bce3389dcc288d1bbb3cc4bb5b3c900e2579e39
Galym <galymsakitzhan@gmail.com>
2023-12-20 20:34:59 +0600

    add callback cart

---
+++ main.js
@@ -0,0 +1,8 @@
+let cartIcon = document.querySelector('#cart-icon')
+let cart = document.querySelector('.cart')
+let closeCart = document.querySelector('#close-cart')
+
+
+cartIcon.addEventListener('click', ()=>{
+    cart.classList.add("active")
+})
